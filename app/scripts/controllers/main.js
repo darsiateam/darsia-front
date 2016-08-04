e3cf8e00203c235cbe074834d1f6e5cb34b796ad
mateosss <mateodemayo@gmail.com>
2016-08-04 15:43:33 -0300

    Initial yeoman setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name darsiaFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the darsiaFrontApp
+ */
+angular.module('darsiaFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
